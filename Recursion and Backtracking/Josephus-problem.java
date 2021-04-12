565967bb4f37cd40d37efe6d46fe6ba97fa6579d
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-04-12 17:57:21 +0530

    Create Josephus-problem.java

---
+++ Recursion and Backtracking/Josephus-problem.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  //0 to n - 1
+  public static int solution(int n, int k){
+    if(n == 1){
+        return 0;
+    }
+    
+    int x = solution(n - 1, k);//recursion
+    int y = (x + k) % n;
+    
+    return y;
+  }
+  
+  public static void main(String[] arg) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int k = scn.nextInt();
+    System.out.println(solution(n,k));
+  }
+
+}
