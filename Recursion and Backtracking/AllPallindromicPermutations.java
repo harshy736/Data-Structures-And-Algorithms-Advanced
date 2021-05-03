870d8b325061f33ccd0e945cd4a3918f0cd9b706
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-05-03 19:17:52 +0530

    Update AllPallindromicPermutations.java

--- Recursion and Backtracking/AllPallindromicPermutations.java
+++ Recursion and Backtracking/AllPallindromicPermutations.java
@@ -3,20 +3,43 @@ import java.util.*;
 
 public class Main {
     
+    public static String reverse(String s){
+        StringBuilder sb = new StringBuilder();
+ 
+        // append a string into StringBuilder
+        sb.append(s);
+ 
+        // reverse StringBuilder
+        sb.reverse();
+ 
+        // return reversed String
+        return sb.toString();
+    }
+    
+    
     //cs - current spot
     //ts - total spot
-	public static void generatepw(int cs, int ts, HashMap fmap, Character oddc, String asf) {
+	public static void generatepw(int cs, int ts, HashMap<Character, Integer> fmap, Character oddc, String asf) {
 	    //permutation completed
         if (cs == ts + 1) {
-        	System.out.println(asf + (oddc == null ? "" : oddc) + reverse(asf));
+            String revs = reverse(asf);//reversed string
+            
+            if(oddc != null){
+                asf += oddc + "";
+            }
+            
+        	System.out.println(asf + revs);
         	return;
         }
     
-        for (char ch : fmap.keySet()) {
-        	if (fmap.get(ch) > 0) {
-        		fmap.put(ch, fmap.get(ch) - 1);
-        		generatepw(cs + 1, ts, fmap,oddc, asf + ch);
-        		fmap.put(ch, fmap.get(ch) + 1);
+        for(char ch : fmap.keySet()){ 
+            int freq = fmap.get(ch);
+            
+            
+        	if (freq > 0) {
+        		fmap.put(ch, freq - 1);
+        		generatepw(cs + 1, ts, fmap, oddc, asf + ch);
+        		fmap.put(ch, freq);
         	}
         }
     }
@@ -24,6 +47,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		String str = scn.next();
+		
 		HashMap<Character, Integer> fmap = new HashMap<>();
 		for (int i = 0; i < str.length(); i++) {
 			char ch = str.charAt(i);
@@ -35,23 +59,20 @@ public class Main {
         Character oddchar = null;
         int length = 0;
         
-        for (int i = 0; i < 26; i++) {
-        	char ch = (char) ('a' + i);
-        	if (fmap.containsKey(ch)) {
-        		int freq = fmap.get(ch);
-        		if (freq % 2 != 0) {
-             		oddchar = ch;
-        		    ofc++;
-        		}
-        		
-        		if (ofc > 1) {//no pallindrome possible
-        		    System.out.println("-1");
-        		    return;
-        		}
-        		
-        		fmap.put(ch, freq / 2);
-        		length += (freq / 2);
-        	}
+        for(char ch : fmap.keySet()){
+    		int freq = fmap.get(ch);
+    		if (freq % 2 != 0) {
+         		oddchar = ch;
+    		    ofc++;
+    		}
+    		
+    		if (ofc > 1) {//no pallindrome possible
+    		    System.out.println("-1");
+    		    return;
+    		}
+    		
+    		fmap.put(ch, freq/2);
+    		length += (freq / 2);
         }
         
         generatepw(1, length, fmap, oddchar, "");
