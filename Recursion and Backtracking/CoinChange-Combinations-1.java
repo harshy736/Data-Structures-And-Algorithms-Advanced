d7646d5d5bf5f9ad1c002e0691a5accf092e25d8
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-05-09 19:09:55 +0530

    Create CoinChange-Combinations-1.java

---
+++ Recursion and Backtracking/CoinChange-Combinations-1.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    //i -> pointer on coins
+    //amtsf -> amt so far
+    public static void coinChange(int i, int[] coins, int amtsf, int tamt, String asf){
+        //base case
+        if(amtsf == tamt){
+            System.out.println(asf + ".");
+            return;
+        }
+        
+        //out of flow
+        if(amtsf > tamt || i == coins.length){
+            return;
+        }
+        
+        int c = coins[i];
+        
+        //coin participate
+        coinChange(i + 1, coins, amtsf + c, tamt, asf + c + "-");
+        
+        //coin does not participate
+        coinChange(i + 1, coins, amtsf, tamt, asf);
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        int amt = Integer.parseInt(br.readLine());
+        
+        coinChange(0, coins, 0, amt, "");
+    }
+}
