7bcc40588b794cebac2f1081cd4be0c0839d8f39
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-08-12 11:18:05 +0530

    Create ConstructBinaryTreeFromInorderAndLevelorderTraversal.java

---
+++ Data-Structures/Trees/ConstructBinaryTreeFromInorderAndLevelorderTraversal.java
@@ -0,0 +1,91 @@
+/*
+1. You are given a partially written function to solve(Refer question video).
+2. Task : Construct Binary Tree from InOrder and LevelOrder Traversal.
+3. you will be given two arrays representing a valid InOrder & LevelOrder of a Binary Tree. Program is required to create a unique Binary Tree.
+*/
+
+import java.util.*;
+
+public class Main {
+  public static Scanner scn = new Scanner(System.in);
+
+  static class TreeNode {
+    int val;
+    TreeNode left = null, right = null;
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+  }
+
+  public static TreeNode buildTree(int[] inorder, int[] levelOrder) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < inorder.length; i++)
+            map.put(inorder[i], i);
+        
+        TreeNode root = new TreeNode(levelOrder[0]);
+        //place all nodes at respective position
+        for (int i = 1; i < levelOrder.length; i++) {
+            helper(root, map, levelOrder[i]);
+        }
+        
+        return root;
+    }
+
+    public static void helper(TreeNode root, Map<Integer, Integer> map, int add) {
+        if (root == null)
+            return;
+            
+        if (map.get(root.val) > map.get(add)) {//lies in left subtree
+            if (root.left == null)
+                root.left = new TreeNode(add);
+            else
+                helper(root.left, map, add);
+        } 
+        else {//lies in right subtree
+            if (root.right == null)
+                root.right = new TreeNode(add);
+            else
+                helper(root.right, map, add);
+        }
+    }
+
+// input_section=================================================
+
+public static void display(TreeNode node) {
+  if (node == null)
+    return;
+
+  StringBuilder sb = new StringBuilder();
+  sb.append((node.left != null ? node.left.val : "."));
+  sb.append(" -> " + node.val + " <- ");
+  sb.append((node.right != null ? node.right.val : "."));
+
+  System.out.println(sb.toString());
+
+  display(node.left);
+  display(node.right);
+
+}
+
+public static void solve() {
+  int n = scn.nextInt();
+
+  int[] InOrder = new int[n];
+  for (int i = 0; i < n; i++)
+    InOrder[i] = scn.nextInt();
+
+  int[] LevelOrder = new int[n];
+  for (int i = 0; i < n; i++)
+    LevelOrder[i] = scn.nextInt();
+
+  TreeNode root = buildTree(InOrder, LevelOrder);
+  display(root);
+}
+
+public static void main(String[] args) {
+  solve();
+}
+
+}
